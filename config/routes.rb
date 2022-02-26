6ea57dec024b23c23510993776dfb58e18fc4a93
makhtar fall <makhtarfall2003@gmail.com>
2022-02-26 17:26:56 +0100

    Generated users contoller + actions + views + routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'index', to: 'users#index', as: :users
+  get 'users/show', to: 'users#show'
 end
